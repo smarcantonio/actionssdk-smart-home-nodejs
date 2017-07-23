8a009ddba7de0b2de70380ba1a90456237d7b7b6
Stephen Marcantonio <stephenmarcantonio@gmail.com>
2017-07-23 18:43:25 +0100

    Following tutorial - adding API key

--- smart-home-provider/cloud/config-provider.js
+++ smart-home-provider/cloud/config-provider.js
@@ -16,7 +16,7 @@ var Config = {};
 Config.devPortSmartHome = "3000";
 Config.smartHomeProviderGoogleClientId = "ZxjqWpsYj3"; // client id that Google will use
 Config.smartHomeProvideGoogleClientSecret = "hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv"; // client secret that Google will use
-Config.smartHomeProviderApiKey = "<API_KEY>"; // client API Key generated on the console
+Config.smartHomeProviderApiKey = "AIzaSyDHkUNYihjD8o5JV-K7G9BsixyuXBZSAAE"; // client API Key generated on the console
 Config.isLocal = false;
 
 function init() {
